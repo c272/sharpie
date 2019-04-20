f959150ab5065cb96c15bfad4b6ba793a78d6e57
C272 <realrussianmanconfirmed@gmail.com>
2019-04-20 20:33:29 +0100

    Update the loader to support live updating.

--- sharpie_loader/Program.cs
+++ sharpie_loader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
 
 namespace sharpie_loader
 {
@@ -9,17 +10,31 @@ namespace sharpie_loader
 
         static void Main(string[] args)
         {
-            // Checking if the user is requesting to update.
+            //Checking if the user is requesting to update.
             if (args.Length>0 && args[0]=="--update")
             {
-                Console.WriteLine("UPDATE DETECTED! :)");
+                //Attempting to grab from the GitHub latest directory.
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        client.DownloadFile("https://github.com/c272/sharpie/raw/master/latest/sharpie_ex.exe", "sharpie_ex.exe");
+                        client.DownloadFile("https://github.com/c272/sharpie/raw/master/latest/sharpie_ex.dll", "sharpie_ex.dll");
+                        Console.WriteLine("Successfully pulled the latest Sharpie build from the master update source.");
+                    } catch
+                    {
+                        //Failed the download.
+                        Console.WriteLine("S_ERR: Failed to pull from master update source, GitHub may be offline or rate limiting you. Please try again later.");
+                    }
+                }
+
                 Environment.Exit(0);
             }
 
-            // Start the child process.
+            //Start the child process.
             Process p = new Process();
 
-            // Redirect the output stream of the child process.
+            //Redirect the output stream of the child process.
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.FileName = "sharpie_ex.exe";
